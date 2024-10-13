82ae3ef7b86c332c578cace5daeac975135547d0
0dminnimda <0dminnimda@gmail.com>
2024-10-14 01:34:46 +0300

    Initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public float destroyTime = 1f;
+    public float waitTime = 0.5f;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        StartCoroutine(InstantiateAndWait());
+    }
+
+    IEnumerator InstantiateAndWait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        GameObject obj = Instantiate(prefab, transform.position, transform.rotation);
+        Destroy(obj, destroyTime);
+    }
+}
---
+++ Assets/Scripts/TestVec.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVec : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector4 vec = new Vector4(1f, 2f, 3f, 4f);
+        Vector3 v = vec;
+        Debug.Log(v);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
